597cb48b68ec85d2587dbd756a8b0b03d5e8e957
Firman Zulkarnain <sco1oter@gmail.com>
2023-11-21 16:49:51 +0700

    Model, DTO, DataAccess, Controllers, addScoped di Program.cs untuk MsCourse

---
+++ DataAccess/MsCourseData.cs
@@ -0,0 +1,166 @@
+﻿using fs_12_team_1_BE.Model;
+using MySql.Data.MySqlClient;
+
+namespace fs_12_team_1_BE.DataAccess
+{
+    public class MsCourseData
+    {
+        private readonly string connectionString = "server=localhost;port=3306;database=fs12apelmusic;user=root;password=";
+
+        public List<MsCourse> GetAll()
+        {
+            List<MsCourse> msCourse = new List<MsCourse>();
+
+            string query = "SELECT * FROM MsCourse";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    connection.Open();
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            msCourse.Add(new MsCourse
+                            {
+                                Id = Guid.Parse(reader["Id"].ToString() ?? string.Empty),
+                                Name = reader["Name"].ToString() ?? string.Empty,
+                                Description = reader["Description"].ToString() ?? string.Empty,
+                                Image = reader["Image"].ToString() ?? string.Empty,
+                                Price = Convert.ToDouble(reader["Price"]),
+                                //CategoryId
+                            });
+                        }
+                    }
+
+                    connection.Close();
+                }
+            }
+
+            return msCourse;
+        }
+
+        public MsCourse? GetById(Guid id)
+        {
+            MsCourse? msCourse = null;
+
+            string query = $"SELECT * FROM MsCourse WHERE Id = '{id}'";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    connection.Open();
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            msCourse = new MsCourse
+                            {
+                                Id = Guid.Parse(reader["Id"].ToString() ?? string.Empty),
+                                Name = reader["Name"].ToString() ?? string.Empty,
+                                Description = reader["Description"].ToString() ?? string.Empty,
+                                Image = reader["Image"].ToString() ?? string.Empty,
+                                Price = Convert.ToDouble(reader["Price"])
+                                //CategoryId
+                            };
+                        }
+                    }
+
+                    connection.Close();
+                }
+            }
+
+            return msCourse;
+        }
+
+        //public bool Insert(Book book)
+        //{
+        //    bool result = false;
+
+        //    string created = book.Created.Date.ToString("yyyy-MM-dd HH:mm:ss");
+        //    string updated = book.Updated.Date.ToString("yyyy-MM-dd HH:mm:ss");
+
+        //    //string query = $"INSERT INTO Books(Id, Title, Description, Author, Stock, Created, Updated) " +
+        //    //    $"VALUES ('{book.Id}','{book.Title}', '{book.Description}', '{book.Author}', {book.Stock}, '{book.Created}', '{book.Updated}')";
+
+        //    string query = $"INSERT INTO Books(Id, Title, Description, Author, Stock, Created, Updated) " +
+        //        $"VALUES ('{book.Id}','{book.Title}', '{book.Description}', '{book.Author}', {book.Stock}, '{created}', '{updated}')";
+
+        //    using (MySqlConnection connection = new MySqlConnection(connectionString))
+        //    {
+        //        using (MySqlCommand command = new MySqlCommand())
+        //        {
+        //            command.Connection = connection;
+        //            command.CommandText = query;
+
+        //            connection.Open();
+
+        //            result = command.ExecuteNonQuery() > 0 ? true : false;
+
+        //            connection.Close();
+        //        }
+        //    }
+
+        //    return result;
+        //}
+
+        //public bool Update(Guid id, Book book)
+        //{
+        //    bool result = false;
+
+        //    string updated = book.Updated.Date.ToString("yyyy-MM-dd HH:mm:ss");
+
+        //    //string query = $"UPDATE Books SET Title = '{book.Title}', Description = '{book.Description}', Author = '{book.Author}', Stock = '{book.Stock}', Updated = '{book.Updated}' " +
+        //    //    $"WHERE Id = '{id}'";
+
+        //    string query = $"UPDATE Books SET Title = '{book.Title}', Description = '{book.Description}', Author = '{book.Author}', Stock = '{book.Stock}', Updated = '{updated}' " +
+        //        $"WHERE Id = '{id}'";
+
+        //    using (MySqlConnection connection = new MySqlConnection(connectionString))
+        //    {
+        //        using (MySqlCommand command = new MySqlCommand())
+        //        {
+        //            command.Connection = connection;
+        //            command.CommandText = query;
+
+        //            connection.Open();
+
+        //            result = command.ExecuteNonQuery() > 0 ? true : false;
+
+        //            connection.Close();
+        //        }
+        //    }
+
+        //    return result;
+        //}
+
+        //public bool Delete(Guid id)
+        //{
+        //    bool result = false;
+
+        //    string query = $"DELETE FROM Books WHERE Id = '{id}'";
+
+        //    using (MySqlConnection connection = new MySqlConnection(connectionString))
+        //    {
+        //        using (MySqlCommand command = new MySqlCommand())
+        //        {
+        //            command.Connection = connection;
+        //            command.CommandText = query;
+
+        //            connection.Open();
+
+        //            result = command.ExecuteNonQuery() > 0 ? true : false;
+
+        //            connection.Close();
+        //        }
+        //    }
+
+        //    return result;
+        //}
+
+    }
+}
---
+++ Model/MsCourse.cs
@@ -0,0 +1,14 @@
+﻿namespace fs_12_team_1_BE.Model
+{
+    public class MsCourse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        public double Price { get; set; }
+        
+        //TODO: add category id foreign key
+
+    }
+}
