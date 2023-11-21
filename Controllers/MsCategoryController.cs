28da4c1be8b69ddc5a1c15657c72a63b7d8f6f5b
Revin Dennis Ramadhan <revinramadhan21@gmail.com>
2023-11-21 20:20:10 +0700

    Membuat model, dataaccess, dan controller untuk Category

---
+++ Controllers/MsCategoryController.cs
@@ -0,0 +1,111 @@
+﻿using fs_12_team_1_BE.DataAccess;
+using fs_12_team_1_BE.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fs_12_team_1_BE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class MsCategoryController : ControllerBase
+    {
+        private readonly MsCategoryData _msCategoryData;
+        public MsCategoryController(MsCategoryData msCategoryData)
+        {
+            _msCategoryData = msCategoryData;
+        }
+
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            List<MsCategory> msCategory = _msCategoryData.GetAll();
+            return Ok(msCategory);
+        }
+
+        [HttpGet("GetById")]
+        public IActionResult Get(Guid id)
+        {
+            MsCategory? msCategory = _msCategoryData.GetById(id);
+
+            if (msCategory == null)
+            {
+                return NotFound("Data not found");
+            }
+
+            return Ok(msCategory); //200
+        }
+
+        //[HttpPost]
+        //public IActionResult Post([FromBody] BookDTO bookDto)
+        //{
+        //    if (bookDto == null)
+        //        return BadRequest("Data should be inputed");
+
+        //    Book book = new Book
+        //    {
+        //        Id = Guid.NewGuid(),
+        //        Title = bookDto.Title,
+        //        Description = bookDto.Description,
+        //        Author = bookDto.Author,
+        //        Stock = bookDto.Stock,
+        //        Created = DateTime.Now,
+        //        Updated = DateTime.Now,
+        //    };
+
+        //    bool result = _bookData.Insert(book);
+
+        //    if (result)
+        //    {
+        //        return StatusCode(201, book.Id);
+        //    }
+        //    else
+        //    {
+        //        return StatusCode(500, "Error occur");
+        //    }
+        //}
+
+        //[HttpPut]
+        //public IActionResult Put(Guid id, [FromBody] BookDTO bookDto)
+        //{
+        //    if (bookDto == null)
+        //        return BadRequest("Data should be inputed");
+
+        //    Book book = new Book
+        //    {
+        //        Title = bookDto.Title,
+        //        Description = bookDto.Description,
+        //        Author = bookDto.Author,
+        //        Stock = bookDto.Stock,
+        //        Updated = DateTime.Now
+        //    };
+
+        //    bool result = _bookData.Update(id, book);
+
+        //    if (result)
+        //    {
+        //        return NoContent();//204
+        //    }
+        //    else
+        //    {
+        //        return StatusCode(500, "Error occur");
+        //    }
+        //}
+
+        //[HttpDelete]
+        //public IActionResult Delete(Guid id)
+        //{
+        //    bool result = _bookData.Delete(id);
+
+        //    if (result)
+        //    {
+        //        return NoContent();
+        //    }
+        //    else
+        //    {
+        //        return StatusCode(500, "Error occur");
+        //    }
+        //}
+
+    }
+}
---
+++ DataAccess/MsCategoryData.cs
@@ -0,0 +1,80 @@
+﻿using fs_12_team_1_BE.Model;
+using MySql.Data.MySqlClient;
+
+namespace fs_12_team_1_BE.DataAccess
+{
+    public class MsCategoryData
+    {
+        private readonly string connectionString = "server=localhost;port=3306;database=fs12apelmusic;user=root;password=";
+
+        public List<MsCategory> GetAll()
+        {
+            List<MsCategory> msCategory = new List<MsCategory>();
+
+            string query = "SELECT * FROM MsCategory";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    connection.Open();
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            msCategory.Add(new MsCategory
+                            {
+                                Id = Guid.Parse(reader["Id"].ToString() ?? string.Empty),
+                                Name = reader["Name"].ToString() ?? string.Empty,
+                                Title = reader["Title"].ToString() ?? string.Empty,
+                                Description = reader["Description"].ToString() ?? string.Empty,
+                                Image = reader["Image"].ToString() ?? string.Empty,
+                                HeaderImage = reader["HeaderImage"].ToString() ?? string.Empty,
+                            });
+                        }
+                    }
+
+                    connection.Close();
+                }
+            }
+
+            return msCategory;
+        }
+
+        public MsCategory? GetById(Guid id)
+        {
+            MsCategory? msCategory = null;
+
+            string query = $"SELECT * FROM MsCategory WHERE Id = '{id}'";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    connection.Open();
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            msCategory = new MsCategory
+                            {
+                                Id = Guid.Parse(reader["Id"].ToString() ?? string.Empty),
+                                Name = reader["Name"].ToString() ?? string.Empty,
+                                Title = reader["Title"].ToString() ?? string.Empty,
+                                Description = reader["Description"].ToString() ?? string.Empty,
+                                Image = reader["Image"].ToString() ?? string.Empty,
+                                HeaderImage = reader["HeaderImage"].ToString() ?? string.Empty,
+                            };
+                        }
+                    }
+
+                    connection.Close();
+                }
+            }
+
+            return msCategory;
+        }
+    }
+}
